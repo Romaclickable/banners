1df67d74e136a2dc797a57752d8ac4560143c72a
RomaClickable <supparom@clickable.design>
2017-09-08 16:22:30 +0300

    all_case

---
+++ outsource/gamer_all_case_builder/assets/case-data.js
@@ -0,0 +1,39 @@
+window.animated_case_data_holder = {
+    
+    "case-0001" : {
+        "in-items" : [
+            'img/1/item1.png',
+            'img/1/item2.png',
+            'img/1/item3.png',
+        ],
+        "gold" : true,
+    },
+    
+    "case-0002" : {
+        "in-items" : [
+            'img/2/item1.png',
+            'img/2/item2.png',
+            'img/2/item3.png',
+            'img/2/item4.png'
+        ],
+        "flash-light-img" : 'img/2/light.png',
+        "sun-img" : 'img/2/sun.png',
+        "gold" : false
+    },
+    
+    "case-0003" : {
+        "in-items" : [
+            'img/3/item1.png',
+        ],
+        "gold" : true
+    },
+    
+    "case-0004" : {
+        "in-items" : [
+            'img/4/item1.png',
+            'img/4/item2.png',
+            'img/4/item3.png',
+        ],
+        "gold" : true
+    },
+}
---
+++ outsource/gamer_all_case_builder/assets/default-case-images.js
@@ -0,0 +1,29 @@
+window.brown_case_default_images = {
+    
+    "case-front-img" : 'def_img_brown/case-front.png',
+    "case-back-img" : 'def_img_brown/case-back.png',
+    
+    "flash-light-img" : 'def_img_brown/light.png',
+    "sun-img" : 'def_img_brown/sun.png',
+
+    "gold-img" : 'def_img_brown/gold.png',
+
+    "button-norm-img" : 'def_img_brown/norm.png',
+    "button-active-img" : 'def_img_brown/hover.png'
+
+}
+
+window.black_case_default_images = {
+    
+    "case-front-img" : 'def_img_black/case-front.png',
+    "case-back-img" : 'def_img_black/case-back.png',
+    
+    "flash-light-img" : 'def_img_black/light.png',
+    "sun-img" : 'def_img_black/sun.png',
+
+    "gold-img" : 'def_img_black/gold.png',
+
+    "button-norm-img" : 'def_img_black/norm.png',
+    "button-active-img" : 'def_img_black/hover.png'
+
+}
